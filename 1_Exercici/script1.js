9b32384be1d53154d8a2217b4b0a9be73bc0b657
Markitos <marksand2001@gmail.com>
2024-02-28 20:40:59 +0100

    Table with Jquery

---
+++ 1_Exercici/script1.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", main);
+
+function main()
+{
+    $("table").css(
+    {
+        margin: "0 auto",
+        "border-collapse": "collapse",
+    });
+
+    $("td").css(
+    {
+        border: "1px solid chocolate",
+        padding: "0.3rem",
+    });
+
+    $("tbody").find("td").each(function()
+    {
+        $(this).addClass("celes");
+        
+        let txt = $(this).text();
+
+        if (teNum(txt))
+        {
+            $(this).css("text-align", "right");
+        }
+    });
+
+    $("table").find("tr").each(function(i)
+    {
+        if (i == 0)
+        {
+            $(this).addClass("hover");
+        }
+        else if ((i)%2 == 0)
+        {
+            $(this).addClass("parell");
+        }
+        else
+        {
+            $(this).addClass("imparell");
+        }
+    });
+
+    $(".hover").click(function()
+    {
+        // Order
+    })
+}
+
+function teNum(txt)
+{
+    return /\d/.test(txt);
+}
